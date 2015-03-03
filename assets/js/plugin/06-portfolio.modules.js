84211a5238687fba3ab6c081b9d2f66d4bd814be
Kev Simpson <hello@kev-simpson.me>
2015-03-03 16:54:28 +0000

    Portfolio: Remove gallery zoom

--- assets/js/plugin/06-portfolio.modules.js
+++ assets/js/plugin/06-portfolio.modules.js
@@ -787,7 +787,7 @@
                 type: 'image',
                 closeOnContentClick: false,
                 closeBtnInside: false,
-                mainClass: 'mfp-with-zoom mfp-img-mobile',
+                mainClass: 'mfp-img-mobile',
                 image: {
                     verticalFit: true,
                     titleSrc: function(item) {
@@ -796,13 +796,6 @@
                 },
                 gallery: {
                     enabled: true
-                },
-                zoom: {
-                    enabled: true,
-                    duration: 300, // don't foget to change the duration also in CSS
-                    opener: function(element) {
-                        return element.find('img');
-                    }
                 }
             });
         }
