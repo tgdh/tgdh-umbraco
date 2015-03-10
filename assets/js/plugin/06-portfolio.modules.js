f2d4eb49669c48cb6ea1952fc35b5af325470a51
Paulius Putna <up614961@myport.ac.uk>
2015-03-10 15:05:52 +0000

    Added pop up for Video embed

--- assets/js/plugin/06-portfolio.modules.js
+++ assets/js/plugin/06-portfolio.modules.js
@@ -798,6 +798,17 @@
                     enabled: true
                 }
             });
+
+            $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+                disableOn: 700,
+                type: 'iframe',
+                mainClass: 'mfp-fade',
+                removalDelay: 160,
+                preloader: false,
+
+                fixedContentPos: false
+            });
+            // console.log('magnific');
         }
 
 
